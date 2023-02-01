adcf7af6113a1de4454e599ebcfa0b988a19dbfa
Zak Smith <zak.smith2@mail.dcu.ie>
2023-02-01 14:04:00 +0000

    Yul mapping constructs added, further work needed on map and array types

--- code/app/Codegen.hs
+++ code/app/Codegen.hs
@@ -3,6 +3,12 @@ import Parser
 -- Beginning the code generation for Fafel to Yul. 
 -- Much more research to be done on Yul constructs and how to map fafel to yul.
 
+genYulLit :: Literal -> String
+genYulLit (IntLit s) = genYulInt s
+genYulLit (FloatLit s) = genYulFloat s
+genYulLit (BoolLit s) = genYulBool s
+genYulLit (AddressLit s) = genYulAddress s
+
 genYulInt :: IntLit -> String
 genYulInt (IntLit s) = show s
 
@@ -33,6 +39,51 @@ genYulCompareOp GreaterEq = "ge"
 genYulCompareOp Equal = "eq"
 genYulCompareOp NotEqual = "ne"
 
+genYulExpr :: Expr -> String
+genYulExpr (BinaryExpr s) = genYulBinaryExpr s 
+genYulExpr (CompareExpr s) = genYulCompareExpr s
+genYulExpr (Literal s) =  genYulLit s
 
+
+genYulBinaryExpr :: BinaryExpr -> String
+genYulBinaryExpr (BinaryExpr op e1 e2) = 
+    genYulBinaryOp op ++ "(" ++ genYulLit e1 ++ "," ++ genYulLit e2 ++ ")"
+
+genYulCompareExpr :: CompareExpr -> String
+genYulCompareExpr (CompareExpr op e1 e2) = 
+    genYulCompareOp op ++ "(" ++ genYulLit e1 ++ "," ++ genYulLit e2 ++ ")"
+
+genYulFuncCall :: FunctionCall -> String
+genYulFuncCall (FunctionCall name args) = 
+    "function " ++ name ++ "(" ++ (intercalate ", " $ map show args) ++ ")"
+
+slotCounter :: IORef Int
+slotCounter = unsafePerformIO $ newIORef 0
+
+getSlot :: IO Int
+getSlot = do
+    counter <- readIORef slotCounter
+    writeIORef slotCounter (counter + 1)
+    return counter
+
+genYulMapping :: MapDecl -> String
+-- should open a slot in storage for the mapping index
+-- and then store the mapping name and position in a symbol table 
+genYulMapping name _ = 
+  let slot = getSlot
+  in "sstore(" ++ show slot ++ ", 0)"
+
+genYulMappingExpr :: MapExpr -> String
+genYulMappingExpr name key = 
+
+
+
+genYulMappingAssign :: MapAssignExpr -> String
+genYulMappingAssign 
+
+genYulList :: ListType -> String
+genYulList l =
+  let slot = getSlot
+  in "sstore(" ++ show slot ++ ", 0)"
 
 
