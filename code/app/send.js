5d2727f7acbd4ad38362f75fa31d85587d69e8b6
Senan Warnock <senan.warnock2@mail.dcu.ie>
2023-02-22 16:43:06 +0000

    add send script

---
+++ code/app/send.js
@@ -0,0 +1,35 @@
+const Web3 = require('web3');
+require('dotenv').config();
+// Replace with your own Web3 provider endpoint
+const web3 = new Web3(process.env.ENDPOINT);
+
+// Replace with the address of the contract you want to interact with
+const contractAddress = '0x42C0480E91f019a267AB64F3b60Ac378E25eaBaE';
+
+// Replace with the function signature you want to call
+const functionSignature = process.argv[2];
+
+// Replace with the argument you want to pass to the function
+const arg = process.argv[3];
+const paddedArg = arg.padStart(64, '0');
+
+const pk = process.env.PRIVATE_KEY;
+
+// Create the raw transaction object
+const txObject = {
+  from: process.env.ADDRESS,
+  to: contractAddress,
+  gas: 300000,
+  data: functionSignature + paddedArg,
+};
+
+// Sign the transaction and send it to the blockchain
+web3.eth.accounts.signTransaction(txObject, pk)
+    .then((signedTx) => {
+        web3.eth.sendSignedTransaction(signedTx.rawTransaction)
+            .on('receipt', console.log)
+            .on('error', console.error);
+    });
+
+//print padded argument
+console.log(paddedArg);
